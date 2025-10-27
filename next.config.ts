9b0de11362163a2149b496b4fcba28512cde23c6
Axel Resendiz <armzmcav@gmail.com>
2025-10-27 15:16:33 -0600

    AddConfig2

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,5 @@
 import type { NextConfig } from "next";
 
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: `
-      default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval'
-        https://www.googletagmanager.com
-        https://www.google-analytics.com
-        https://www.googleadservices.com
-        https://www.google.com
-        https://www.gstatic.com;
-      connect-src 'self'
-        https://www.google-analytics.com
-        https://www.googletagmanager.com
-        https://www.googleadservices.com;
-      img-src 'self' data:
-        https://www.google-analytics.com
-        https://www.googletagmanager.com
-        https://stats.g.doubleclick.net;
-      frame-src
-        https://www.googletagmanager.com
-        https://www.google.com
-        https://www.google.com/ads;
-    `.replace(/\n/g, " "), // elimina saltos de línea
-  },
-];
-
 const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', 'backend-ecommerce-87y0.onrender.com', 'res.cloudinary.com'],
@@ -50,14 +23,7 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
