2182d053a3e2bfdf95fa15152aa5909df76a32c6
Axel Resendiz <armzmcav@gmail.com>
2025-12-12 13:03:19 -0600

    Update NavMobile

--- components/navMobile.tsx
+++ components/navMobile.tsx
@@ -102,16 +102,7 @@ const NavMobile = ({ isMenuOpen, toggleMenu }: NavMobileProps) => {
                     >
                         Productos Top
                     </button>
-                    <button
-                        onClick={() => {
-                            router.push("/blog");
-                            toggleMenu();
-                        }}
-                        className="block w-full text-left px-4 py-2 text-gray-700 hover:text-blue-600 
-                       hover:bg-blue-50 rounded-lg transition-all duration-200"
-                    >
-                        Blog
-                    </button>
+                   
 
                     <button
                         onClick={() => {
