7d2763a4eb7c08ea8b95b1c5dd71159f896feb9e
Airrmcav <armzmcav@gmail.com>
2026-01-28 11:45:52 -0600

    Refactor next.config.ts for better readability

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,41 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'backend-ecommerce-87y0.onrender.com', 'res.cloudinary.com'],
+    domains: [
+      "localhost",
+      "backend-ecommerce-87y0.onrender.com",
+      "res.cloudinary.com",
+    ],
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'backend-ecommerce-87y0.onrender.com',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "backend-ecommerce-87y0.onrender.com",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
     ],
   },
 
-
+  async redirects() {
+    return [
+      {
+        source: "/product/:slug*",
+        destination: "/:slug*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
