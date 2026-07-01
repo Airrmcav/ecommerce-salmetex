7f268b9876b44c20cf60988175b3520ef3874815
AxelResendizRzm <ivanramres@gmail.com>
2026-07-01 16:30:21 -0600

    refactor 3

--- env.d.ts
+++ env.d.ts
@@ -7,3 +7,13 @@ declare namespace NodeJS {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
   }
 }
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.css" {
+  const css: string;
+  export default css;
+}
